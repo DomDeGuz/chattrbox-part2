0c4f10432821e74f1eb10ac8543ca5db2fe97c20
domdeguz <d0ub13d@csu.fullerton.edu>
2018-04-18 18:01:05 -0800

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
-  "env": {
-    "browser": true
-  },
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "windows"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
+    "env": {
+      "browser": true,
+    "es6": true
+},
+"parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+},
+"extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+          "error",
+          "always"
+        ],
+    "no-console": "off"
+    }
 };
